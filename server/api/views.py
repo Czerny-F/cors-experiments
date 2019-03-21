b3439563f7a0b76f617a183bf53e8e47143b9737
Czerny-F <dummy00.cz@gmail.com>
2019-03-22 02:26:06 +0900

    server: change response a little

--- server/api/views.py
+++ server/api/views.py
@@ -5,4 +5,4 @@ from rest_framework.response import Response
 @api_view(['GET', 'POST'])
 def cors(request):
     """API endpoint for experimental CORS"""
-    return Response({'response': 'cors'})
+    return Response({'answer': 'cors'})
